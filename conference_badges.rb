235e762b145b19864c93931175bff31e1e119713
AndrewKapsa <andrew.kapsa1@gmail.com>
2022-04-12 11:39:22 -0400

    commit tree 9ca376a736e26a6a2cb378b82e0a76a765978756

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    names.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms(names)
+    names.map.with_index(1) do |name, room_num|
+        "Hello, #{name}! You'll be assigned to #{room_num}!"
+    end
+end
